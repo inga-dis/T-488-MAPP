0d62804ed6d88ae04b63f9593c19aceab75202e2
inga-dis <ingadis28@gmail.com>
2022-11-22 18:50:16 +0000

    main page- unfinished

--- toddler/src/views/main/styles.js
+++ toddler/src/views/main/styles.js
@@ -2,9 +2,8 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     text: {
-        position: 'absolute',
-        top: 15,
-        right: 15,
+        position: 'float',
+        padding: 50 ,    
         fontSize: 16,
         backgroundColor: "pink"
     }
