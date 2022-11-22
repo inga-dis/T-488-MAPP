06e2eb25d3baa975296b330c32c71cff07150a6e
inga-dis <ingadis28@gmail.com>
2022-11-22 17:11:03 +0000

    Board component - not finished. Also added logo and color theme.

---
+++ toddler/src/components/boards/index.js
@@ -0,0 +1,12 @@
+import React from ' rect'
+import { View, Text } from 'react-native';
+
+const Board = () => {
+    return (
+        <View>
+            <Text>Board goes here</Text>
+        </View>
+    )
+}
+
+export default Board;
