43272b4421ad18ef522fcba98dc3ff9a16f8af1f
inga-dis <ingadis28@gmail.com>
2022-11-23 09:36:08 +0000

    Koma upp teningingum

--- toddler/App.js
+++ toddler/App.js
@@ -1,25 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import logo from './src/resources/logo.png'
-import Main from './src/views/main';
+import 'react-native-gesture-handler';
+import AppContainer from './src/routes';
+import moment from 'moment';
+moment.locale('en');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>???</Text>
-      <Image source={logo} />
-      <StatusBar style='auto' />
-      <Main/>
-    </View>
+    <AppContainer />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
--- toddler/src/views/main/index.js
+++ toddler/src/views/main/index.js
@@ -1,34 +1,12 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import logo from '../../resources/img.png';
 
-import React from "react";
-import {View, Text, FlatList, SafeAreaView} from 'react-native';
-import styles from './styles';
-import data from '../../resources/data.json';
+const Main = () => {
+  <View>
+      <Image source={logo} />
+      <Text>HELLO WORLD!</Text>
+  </View> 
+};
 
-const word = data.boards;
-
-const Item = ({ name }) => (
-    <View style={styles.item}>
-      <Text style={styles.name}>{name}</Text>
-    </View>
-  );
-
-  const App = () => {
-    const renderItem = ({ item }) => (
-      <Item name={item.name} />
-    );
-  
-    return (
-        <SafeAreaView style={styles.container}>
-            <FlatList
-            data={word}
-            renderItem={renderItem}
-            keyExtractor={item => item.id}
-            />
-        </SafeAreaView>
-    );
-  }
-
-
-
-export default App;
-
+export default Main; 
