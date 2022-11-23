1647492f85e9c12261e817d5ff146dd52ffc5f56
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-11-23 10:02:37 +0000

    main index

--- toddler/src/views/main/index.js
+++ toddler/src/views/main/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-import logo from '../../resources/img.png';
 
-const Main = ({ navigation: { navigate } }) => {
-  <View>
-      <Image source={logo} />
-      <Text>HELLO WORLD!</Text>
-  </View> 
-};
+import React from "react";
+import {View, Text, FlatList, Image, TouchableHighlight} from 'react-native';
+import styles from './styles';
+import data from '../../resources/data.json';
+import logo from '../../resources/logo.png'
 
-export default Main; 
+const word = data.boards;
+
+const onPress = () => (
+    <View>
+        <Text>hah</Text>
+    </View>
+    
+);
+
+const Item = ({ name }) => (
+    <View style={styles.item}>
+        <TouchableHighlight onPress={onPress}>
+    {/* <Image source={logo} /> */}
+      <Text style={styles.name}>{name}</Text>
+      </TouchableHighlight>
+    </View>
+  );
+
+  const App = () => {
+    const renderItem = ({ item }) => (
+        
+      <Item name={item.name} />
+    );
+    const onPress = () => setCount(count + 1);
+  
+    return (
+        <View>
+                <View style={styles.container}>
+                    <FlatList
+                    data={word}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    />
+                    
+                </View>
+           
+        </View>
+    );
+  }
+
+export default App;
