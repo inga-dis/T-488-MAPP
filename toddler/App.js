26e91c361affc4b88419b38eed9a7a56c9833b46
inga-dis <ingadis28@gmail.com>
2022-11-23 12:46:26 +0000

    Setja upp teningingar

--- toddler/App.js
+++ toddler/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+//import AppContainer from './src/routes';
 import { StyleSheet, Text, View, Image } from 'react-native';
-//import logo from './src/resources/logo.png'
-import Main from './src/views/main';
+import Main from './src/views/Main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>???</Text>
-      {/* <Image source={logo} /> */}
-      <StatusBar style='auto' />
-      <Main/>
+    <View style={styles.container }>
+      <Main />
     </View>
+
 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
--- toddler/src/views/main/index.js
+++ toddler/src/views/main/index.js
@@ -1,50 +1,62 @@
 import React from "react";
-import {View, Text, FlatList, Image, TouchableHighlight} from 'react-native';
+import {View, Text, Image, TouchableHighlight } from 'react-native';
 import styles from './styles';
-import data from '../../resources/data.json';
-import logo from '../../resources/logo.png'
+//import data from '../../resources/data.json';
+import Logo from '../../resources/logo.png'
 
-const word = data.boards;
-
-const onPress = () => (
-    <View>
-        <Text>hah</Text>
-    </View>
-    
+const Main = () => (
+  <View style={styles.container}>
+    <Image source={Logo} style={styles.logoStyle}/>
+    <Text>Testing some more</Text>
+    <TouchableHighlight style={styles.button}>
+      <Text style={styles.butttonText}>Board</Text>
+    </TouchableHighlight>
+  </View>
 );
 
-const Item = ({ name }) => (
-    <View style={styles.item}>
-        <TouchableHighlight onPress={onPress}>
-    {/* <Image source={logo} /> */}
-      <Text style={styles.name}>{name}</Text>
-      </TouchableHighlight>
-    </View>
-  );
+export default Main;
 
-  const App = () => {
-    const renderItem = ({ item }) => (
+// const word = data.boards;
+
+// const onPress = () => (
+//     <View>
+//         <Text>hah</Text>
+//     </View>
+    
+// );
+
+// const Item = ({ name }) => (
+//     <View style={styles.item}>
+//         <TouchableHighlight onPress={onPress}>
+//     {/* <Image source={logo} /> */}
+//       <Text style={styles.name}>{name}</Text>
+//       </TouchableHighlight>
+//     </View>
+//   );
+
+//   const App = () => {
+//     const renderItem = ({ item }) => (
         
-      <Item name={item.name} />
-    );
-    const onPress = () => setCount(count + 1);
+//       <Item name={item.name} />
+//     );
+//     const onPress = () => setCount(count + 1);
   
-    return (
-        <View>
-                <View style={styles.container}>
-                    <FlatList
-                    data={word}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.id}
-                    />
+//     return (
+//         <View>
+//                 <View style={styles.container}>
+//                     <FlatList
+//                     data={word}
+//                     renderItem={renderItem}
+//                     keyExtractor={item => item.id}
+//                     />
                     
-                </View>
+//                 </View>
            
-        </View>
-    );
-  }
+//         </View>
+//     );
+//   }
 
 
-export default App;
+// export default App;
 
 
