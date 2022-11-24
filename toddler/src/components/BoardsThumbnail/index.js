4429f5fff539717cbc8ceb9afd2c8d9869f44579
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-11-24 10:35:27 +0000

    style for boards, complete

--- toddler/src/components/BoardsThumbnail/index.js
+++ toddler/src/components/BoardsThumbnail/index.js
@@ -3,12 +3,12 @@ import { Image, View, Text } from "react-native";
 import styles from "./styles";
 
 const BoardsThumbnail = ({ id, name, thumbnailPhoto }) => (
-    <View> 
+    <View style={styles.board}> 
         <Image
             style={styles.image}
             resizeMode="cover"
             source={{ uri: thumbnailPhoto}} />
-        <Text>{name}:{id}</Text>
+        <Text style={styles.boardText}>{name}</Text>
     </View>
 );
 
