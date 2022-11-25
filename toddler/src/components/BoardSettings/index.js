b8ed0621cb0eddf725bdcaa5448368bb210e838e
gudlaugig <gudlaug20@ru.is>
2022-11-25 11:25:32 +0000

    Settings kommentað út

--- toddler/src/components/BoardSettings/index.js
+++ toddler/src/components/BoardSettings/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import {View, Text, Image, TouchableHighlight, Button } from 'react-native';
-import styles from './styles';
+// import React from "react";
+// import {View, Text, Image, TouchableHighlight, Button } from 'react-native';
+// import styles from './styles';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Button title="Change name" />
-      <Button title="Change color" />
-      <Button title="Delete" />
-    </View>
-  );
-};
-
-
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Button title="Change name" />
+//       <Button title="Change color" />
+//       <Button title="Delete" />
+//     </View>
+//   );
+// };
 
 
-export default App;
+
+
+// export default App;
