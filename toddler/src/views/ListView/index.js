554df823d02299fd3cd44d795ec035cd1e1f6595
gudlaugig <gudlaug20@ru.is>
2022-11-25 18:31:40 +0000

    viewlist added

---
+++ toddler/src/views/ListView/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {View, Text,ScrollView } from 'react-native';
+// import Swiper from 'react-native-swiper'
+import styles from './styles';
+
+const ListView = () => (
+
+    <ScrollView horizontal={true}>
+        <View style={styles.TaskList}>
+            <Text style={styles.TaskListName}>DUMMY DATA</Text>
+            <Text style={styles.Task}>TODO</Text>
+            <Text style={styles.Task}>TODOO</Text>
+            <Text style={styles.Task}>TOODOO</Text>
+            <Text style={styles.Task}>TODO</Text>
+            <Text style={styles.Task}>TODOO</Text>
+            <Text style={styles.Task}>TOODOO</Text>
+        </View> 
+        <View style={styles.TaskList}>
+            <Text style={styles.TaskListName}>DUMMY DATA</Text>
+            <Text style={styles.Task}>TODO</Text>
+            <Text style={styles.Task}>TODOO</Text>
+            <Text style={styles.Task}>TOODOO</Text>
+        </View> 
+    </ScrollView>
+    
+)
+
+export default ListView;
--- toddler/src/views/MainView/index.js
+++ toddler/src/views/MainView/index.js
@@ -3,7 +3,6 @@ import {View, Text, Image, TouchableHighlight } from 'react-native';
 import styles from './styles';
 import Logo from '../../resources/logo.png'
 
-
 const MainView = ({ navigation: { navigate } }) => (
   <View style={styles.container}>
         <Image source={Logo} style={styles.logoStyle} />
@@ -13,59 +12,7 @@ const MainView = ({ navigation: { navigate } }) => (
             style={styles.button}>
             <Text style={styles.butttonText}>Board</Text>
         </TouchableHighlight>
-        {/* <TouchableHighlight 
-            onPress={() => navigate('BoardSettings')}
-            style={styles.button}>
-            <Text style={styles.butttonText}>Settings</Text>
-        </TouchableHighlight> */}
   </View>
 );
 
-
-
 export default MainView;
-
-// const word = data.boards;
-
-// const onPress = () => (
-//     <View>
-//         <Text>hah</Text>
-//     </View>
-    
-// );
-
-// const Item = ({ name }) => (
-//     <View style={styles.item}>
-//         <TouchableHighlight onPress={onPress}>
-//     {/* <Image source={logo} /> */}
-//       <Text style={styles.name}>{name}</Text>
-//       </TouchableHighlight>
-//     </View>
-//   );
-
-//   const App = () => {
-//     const renderItem = ({ item }) => (
-        
-//       <Item name={item.name} />
-//     );
-//     const onPress = () => setCount(count + 1);
-  
-//     return (
-//         <View>
-//                 <View style={styles.container}>
-//                     <FlatList
-//                     data={word}
-//                     renderItem={renderItem}
-//                     keyExtractor={item => item.id}
-//                     />
-                    
-//                 </View>
-           
-//         </View>
-//     );
-//   }
-
-
-// export default App;
-
-
