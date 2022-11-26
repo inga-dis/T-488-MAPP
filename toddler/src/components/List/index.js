7a20128ee81f39b74feedac716e176569621fc89
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-11-26 13:45:16 +0000

    delete button, console.log

--- toddler/src/components/List/index.js
+++ toddler/src/components/List/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, TouchableOpacity  } from "react-native";
+import { View, Text, TouchableOpacity, Button  } from "react-native";
 import styles from "./styles";
 
 
@@ -12,6 +12,11 @@ const List = ({ list }) => {
             onPress={() => navigate('ListView', { listName: list.name, listId: list.id, color: list.color})}>
             <View style={styles.list}> 
                 <Text style={styles.listText}>{list.name}</Text>
+            
+            <Button 
+                title="X"
+                onPress={() => console.log(list.name)}
+            />
             </View>
     </TouchableOpacity>
     );
