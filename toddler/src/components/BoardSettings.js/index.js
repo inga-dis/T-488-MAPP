6801dcbee2ed2e9beb07a681a7e3876e86122676
gudlaugig <gudlaug20@ru.is>
2022-11-27 18:03:29 +0000

    Delete board + clean up

--- toddler/src/components/BoardSettings.js/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {text, View} from "react-native"
-
-<View style={styles.container}>
-      <TextInput placeholder="Board"/>
-      <Button title="Add" onPress={handleSubmit}/>
-</View>
