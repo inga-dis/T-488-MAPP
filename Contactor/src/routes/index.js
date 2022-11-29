c27672c873ce394e63e5bcfeafac6db899529c8e
inga-dis <ingadis28@gmail.com>
2022-11-29 21:56:38 +0000

    file system -incomplete

--- Contactor/src/routes/index.js
+++ Contactor/src/routes/index.js
@@ -10,18 +10,24 @@ const Stack = createStackNavigator();
 // Views
 import Main from "../view/Main";
 import Contacts from "../view/Contacts";
+// testing view
+import Test from "../view/Test";
 
 
 const Routes = () => (
     <NavigationContainer >
-        <Stack.Navigator initialRouteName="Main" component={Main}>
-            <Stack.Screen name="main" component={Main} options={{
-            }} />
+        <Stack.Navigator initialRouteName="Test" component={Test}>
+            <Stack.Screen name="Test" component={Test} options={{
+                    title: 'Test',
+                }} />
+        {/* <Stack.Navigator initialRouteName="Main" component={Main}> */}
+            {/* <Stack.Screen name="main" component={Main} options={{
+            }} /> */}
+
             <Stack.Screen name="Contacts" component={Contacts} options={{
                 title: 'Contacts',
 
             }} />
-
         </Stack.Navigator>
     </NavigationContainer>
 );
