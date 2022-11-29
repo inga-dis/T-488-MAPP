f618dcaaddcaebc476a419731d5f105dcc2e8be0
gudlaugig <gudlaug20@ru.is>
2022-11-29 09:32:49 +0000

    header

---
+++ Contactor/src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import {  lightBlue, lightPurple, lightYellow, white} from '../../styles/colors';
+// const windowWidth = Dimensions.get('window').width;
+
+export default StyleSheet.create({
+    header: {
+        backgroundColor: lightYellow,
+        borderBottomWidth: 0,
+        height: 10,
+    },
+});
