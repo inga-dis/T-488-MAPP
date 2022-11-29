04b5a17889c514afeae48a5af482de593110f1ca
gudlaugig <gudlaug20@ru.is>
2022-11-29 11:43:36 +0000

    fixing routes

--- Contactor/src/view/Main/index.js
+++ Contactor/src/view/Main/index.js
@@ -6,7 +6,9 @@ import Logo from '../../components/Logo/telephone.png';
 
 
 
+
 const Main = ({ navigation: { navigate } }) => (
+    <Header style = {styles.header} name = "Main"></Header>,
     <View style={styles.container}>
             <Text style={styles.header}>Your personal contactor</Text>
             <Image source={Logo} style={styles.logoStyle} />
