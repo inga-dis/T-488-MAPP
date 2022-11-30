abc5965d963d92aa9e88cc52afdb004d59f7ce95
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-11-30 17:59:57 +0000

    taka mynd, merge vesen lagað

--- Contactor/src/components/ContactProfile/index.js
+++ Contactor/src/components/ContactProfile/index.js
@@ -1,25 +1,41 @@
 import React from 'react'
 // import { useNavigation } from '@react-navigation/native'
-import { Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import call from 'react-native-phone-call'
+import * as imageService from '../../services/imageService'
+import { useNavigation } from '@react-navigation/native'
 
 const ContactProfile = () => {
+    const { navigate } = useNavigation();
+    const takePhoto = async () => {
+        const photo = await imageService.takePhoto();
+        console.log(photo)
+    };
+
     const triggerCall = () => {
         const args = {
-            number: '8451466', // Use commas to add time between digits.
+            number: '7766877', // Use commas to add time between digits.
             prompt: false
         }
         call(args).catch(console.error)
     }
 
     return (
-        <TouchableOpacity onPress={triggerCall}>
-            {/* <Image
-                style={styles.image}
-                resizeMode="cover"
-                source={{ uri: board.thumbnailPhoto}} /> */}
-            <Text style={{ fontSize: 40 }}>HRINGJA</Text>
-        </TouchableOpacity>
+        <View>
+            <TouchableOpacity onPress={triggerCall}>
+                {/* <Image
+                    style={styles.image}
+                    resizeMode="cover"
+                    source={{ uri: board.thumbnailPhoto}} /> */}
+                <Text style={{ fontSize: 40 }}>HRINGJA</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress ={() => navigate ('Contacts')}>
+                <Text style={styles.option}>Upload image</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress ={() => takePhoto()}>
+                <Text style={styles.option}>Capture image</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
 
