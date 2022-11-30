60cc134cde8a74dfc2daee18a13f602f5c20cb39
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-11-30 17:39:39 +0000

    TAÐ ER HÆGT AÐ HRINGJA!!!!!

--- Contactor/src/components/ContactProfile/index.js
+++ Contactor/src/components/ContactProfile/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { Image, View, Text, TouchableOpacity } from 'react-native'
+// import { useNavigation } from '@react-navigation/native'
+import { Text, TouchableOpacity } from 'react-native'
+import call from 'react-native-phone-call'
 
 const ContactProfile = () => {
+    const triggerCall = () => {
+        const args = {
+            number: '8451466', // Use commas to add time between digits.
+            prompt: false
+        }
+        call(args).catch(console.error)
+    }
+
     return (
-        <View>
+        <TouchableOpacity onPress={triggerCall}>
             {/* <Image
                 style={styles.image}
                 resizeMode="cover"
                 source={{ uri: board.thumbnailPhoto}} /> */}
-            <Text>NAME</Text>
-            <Text>IMAGE</Text>
-            <Text>PHONENUMBER</Text>
-        </View>
+            <Text style={{ fontSize: 40 }}>HRINGJA</Text>
+        </TouchableOpacity>
     )
 }
 
