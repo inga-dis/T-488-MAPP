0b2c37e31dab4605f8dd9d00202a442e41abe2aa
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-12-03 15:32:26 +0000

    modal stíll

---
+++ Contactor/src/components/Modal/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import { lightYellow } from '../../styles/colors'
+
+export default StyleSheet.create({
+    modal: {
+        backgroundColor: lightYellow,
+        marginVertical: 250,
+        borderRadius: 5
+    }
+})
