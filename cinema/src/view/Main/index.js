fbfd19b9e4443c2211e1fe923a88fc873b998d1e
gudlaugig <gudlaug20@ru.is>
2022-12-06 15:33:42 +0000

    Cinema button routing and styling

--- cinema/src/view/Main/index.js
+++ cinema/src/view/Main/index.js
@@ -2,9 +2,10 @@
 import React from 'react'
 import { ScrollView, View, Text, Image, TouchableOpacity } from 'react-native'
 import styles from './styles'
+import CinemaButton from '../../components/CinemaButton'
 
 const Main = ({ navigation: { navigate } }) => (
-    <ScrollView>
+    <ScrollView style={styles.container}>
         <View style={styles.mainHeader}>
             <TouchableOpacity style={styles.header}>
                 <Text style={styles.headerText}>Í sýningu</Text>
@@ -16,36 +17,7 @@ const Main = ({ navigation: { navigate } }) => (
                 <Text style={styles.headerText}>Bíóhús</Text>
             </TouchableOpacity>
         </View>
-        <View  style={styles.container}>
-        <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Sambíó</Text>
-            <Text style={styles.theaterUndertext}>www.sambio.is</Text>
-       </TouchableOpacity> 
-       <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Sambíó</Text>
-            <Text style={styles.theaterUndertext}>www.Smarabio.is</Text>
-        </TouchableOpacity> 
-        <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Háskólabíó</Text>
-            <Text style={styles.theaterUndertext}>www.Haskolabio.is</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Háskólabíó</Text>
-            <Text style={styles.theaterUndertext}>www.Haskolabio.is</Text>
-        </TouchableOpacity> 
-        <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Háskólabíó</Text>
-            <Text style={styles.theaterUndertext}>www.Haskolabio.is</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Háskólabíó</Text>
-            <Text style={styles.theaterUndertext}>www.Haskolabio.is</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.theater} onPress ={() => navigate('Cinema')}>
-            <Text style={styles.theatertext}>Háskólabíó</Text>
-            <Text style={styles.theaterUndertext}>www.Haskolabio.is</Text>
-        </TouchableOpacity>
-        </View>
+        <CinemaButton></CinemaButton>
     </ScrollView>
 )
 
