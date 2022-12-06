3654afc1140dfd389a44e8d3d8d5d8b9f52d1700
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-12-06 10:52:54 +0000

    tengin bætt við moviescreen

--- cinema/src/components/CinemaProfile/index.js
+++ cinema/src/components/CinemaProfile/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 const Cinema = () => {
-
+    const { navigate } = useNavigation()
     return (
         <View>
-            <TouchableOpacity>
+            <TouchableOpacity onPress ={() => navigate('Movie')}>
                 <Text>Hér kemur cinema</Text>
                 <Text>Sambíó + About</Text>
                 <Text>Og allt sem er í bíó í Sambíó</Text>
