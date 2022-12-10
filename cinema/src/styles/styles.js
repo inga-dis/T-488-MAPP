c8a8c9b9fd8aef773c574548e7ad1d1903dfcd6e
DESKTOP-CC9ITHR\Katrin Lilja <katrinp19@ru.is>
2022-12-10 14:28:24 +0000

    laga meira style

--- cinema/src/styles/styles.js
+++ cinema/src/styles/styles.js
@@ -13,6 +13,34 @@ export default StyleSheet.create({
     bottom: {
         marginBottom: 20
     },
+//Header:
+    mainHeader: {
+        flex: 3,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    header: {
+        backgroundColor: '#472E1F',
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+        borderWidth: 1,
+        borderColor: 'rgba(50, 23, 13, 0.5)'
+    },
+    headerText: {
+        color: gold, 
+        fontSize: 15
+    }, 
+    cinemaheader: {
+        backgroundColor: '#472E1F',
+        borderColor: 'rgba(50, 23, 13, 0.5)',
+        paddingVertical: 10
+    },
+    cinemaName: {
+        fontSize: 30,
+        fontWeight: 'bold', 
+        color: lightBlue, 
+        textAlign: 'center'
+    },
 
 //Buttons:
     button: {
